36c19704efdfbfe5cc07eae173950ab057a6bae9
christopher639 <bundichristopher639@gmail.com>
2024-07-23 17:29:13 +0300

    LErnin JS

--- domscript.js
+++ domscript.js
@@ -9,5 +9,21 @@ function subscribe(){
     }
    });
   }
-
   subscribe();//calling the subscription button
+  //amazon calculater
+  function calculateTotal(){
+    let orderTotal =document.querySelector('.js-cost-input');
+    
+    document.querySelector('.btn-cal').addEventListener('click',()=>{
+      console.log(  document.querySelector('.js-cost-input'));
+      let cost =orderTotal.value;
+      if(cost < 40){
+        document.querySelector('.total').innerHTML = cost + 10;
+      }
+      else {
+        document.querySelector('.total').innerHTML = cost;
+      }
+    
+    })
+  }
+  calculateTotal();
